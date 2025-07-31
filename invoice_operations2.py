d8ca31f6aa8cca08cb95f15ad527ae14eebfe424
AngelLinux <angelgabrielquino18@gmail.com>
2025-07-31 01:27:43 -0500

    8. Punto en q no funciona el driver de selenium

--- invoice_operations2.py
+++ invoice_operations2.py
@@ -5,11 +5,17 @@ import browser_automation
 import time
 
 # Asegurarse de que el navegador esté configurado
-driver = browser_automation.driver   # asume que ya lo inicializaste en main.py
+#driver = browser_automation.driver   # asume que ya lo inicializaste en main.py
 
 
 def login_to_system():
     print("1. Introducción de credenciales")
+
+    driver = browser_automation.driver
+    if driver is None:
+        raise RuntimeError("Driver no inicializado. Llama a setup_browser() antes de login_to_system.")
+    print("1. Introducción de credenciales")
+
     try:
         print("     Iniciando...")
         driver.get("https://api-seguridad.sunat.gob.pe/v1/clientessol/4f3b88b3-d9d6-402a-b85d-6a0bc857746a/oauth2/loginMenuSol?originalUrl=https://e-menu.sunat.gob.pe/cl-ti-itmenu/AutenticaMenuInternet.htm&state=rO0ABXNyABFqYXZhLnV0aWwuSGFzaE1hcAUH2sHDFmDRAwACRgAKbG9hZEZhY3RvckkACXRocmVzaG9sZHhwP0AAAAAAAAx3CAAAABAAAAADdAAEZXhlY3B0AAZwYXJhbXN0AEsqJiomL2NsLXRpLWl0bWVudS9NZW51SW50ZXJuZXQuaHRtJmI2NGQyNmE4YjVhZjA5MTkyM2IyM2I2NDA3YTFjMWRiNDFlNzMzYTZ0AANleGVweA==")  # Reemplaza con la URL correcta  # URL completa
